faae2818d9c093af65ed2a017d5fbcd526f917cd
pharlain <pharlain@users.noreply.github.com>
2020-03-04 20:06:09 +0000

    First Division

---
+++ TextBasedGame/game.js
@@ -0,0 +1,26 @@
+function playerObject(username, health, currentRoom, [], stats)
+{
+
+}
+function statsObjects(areasExplored, endingAchieved)
+{
+
+}
+function roomObject(type, description,[], [], [], [])
+{
+
+}
+function itemObject(name, type, interactions)
+{
+
+}
+function interactableObject([])
+{
+
+}
+var player = new playerObject();
+var stats = new statsObject();
+var rooms = [];
+var item = new itemObject();
+var interactable =  new interactableObject();
+
