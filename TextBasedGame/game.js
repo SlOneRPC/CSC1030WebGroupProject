d665cc3aac9c23f194ed24e4371c756fc5f04be2
pharlain <pharlain@users.noreply.github.com>
2020-03-10 11:09:06 +0000

    dump

--- TextBasedGame/game.js
+++ TextBasedGame/game.js
@@ -1,4 +1,18 @@
-function playerObject(username, health, currentRoom, [], stats)
+var player = new playerObject(document.getElementById("name").value, charHealth, document.getElementById("charClass") inventory, stats);
+var stats = new statsObject();
+var inventory = [];
+var item = new itemObject();
+var interactable =  new interactableObject();
+
+var rooms = [];
+
+var enemies = [];
+var exits = [];
+var roomItems = [];
+var interactableRoomObjects = [];
+var verb = [];
+
+function playerObject(username, health, class, currentRoom, inventory, stats)
 {
 
 }
@@ -6,7 +20,7 @@ function statsObjects(areasExplored, endingAchieved)
 {
 
 }
-function roomObject(type, description,[], [], [], [])
+function roomObject(type, description, enemies, exits, roomItems, interactableRoomObjects)
 {
 
 }
@@ -14,13 +28,45 @@ function itemObject(name, type, interactions)
 {
 
 }
-function interactableObject([])
+function interactableObject(verb)
 {
 
 }
-var player = new playerObject();
-var stats = new statsObject();
-var rooms = [];
-var item = new itemObject();
-var interactable =  new interactableObject();
 
+function charHealth()
+{
+  var className = document.getElementById("charClass");
+  if (className == "Hacker")
+  {
+    return 100;
+  }
+  else if (classHealth == "Engineer")
+  {
+    return 100;
+  }
+  else
+  {
+    return 120;
+  }
+}
+
+function charStart()
+{
+  var className = document.getElementById("charClass");
+  if (className == "Hacker")
+  {
+    return ;
+  }
+  else if (classHealth == "Engineer")
+  {
+    return ;
+  }
+  else
+  {
+    return ;
+  }
+}
+function addItemInventory(item)
+{
+  
+}
