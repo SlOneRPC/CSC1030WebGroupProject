824bcfdb18717dd6a174943e9bfeb4d5f5dc8970
pharlain <pharlain@users.noreply.github.com>
2020-03-10 15:49:11 +0000

    Fixed the external javascript

--- TextBasedGame/javascript/game.js
+++ TextBasedGame/javascript/game.js
@@ -1,9 +1,7 @@
-var player = new playerObject(document.getElementById("name").value, charHealth(), document.getElementById("charClass") inventory, stats);
-var stats = new statsObject();
+var stats = new statsObject(1, 1, 1, 1);
 var inventory = [];
-var item = new itemObject();
-var interactable =  new interactableObject();
-
+var item = new itemObject(1,1,11);
+var interactable =  new interactableObject(4);
 
 var rooms = [];
 
@@ -13,11 +11,11 @@ var roomItems = [];
 var interactableRoomObjects = [];
 var verb = [];
 
-function playerObject(username, health, class, currentRoom, inventory, stats)
+function playerObject(username, health, charClass, currentRoom, inventory, stats)
 {
 
 }
-function statsObjects(areasExplored, endingAchieved, enemiesDefeated, timeLeft,)
+function statsObjects(areasExplored, endingAchieved, enemiesDefeated, timeLeft)
 {
 
 }
@@ -53,18 +51,17 @@ function charHealth()
 
 function charStart()
 {
-  var className = document.getElementById("charClass");
-  if (className == "Hacker")
+  if (document.getElementById("Hacker").checked)
   {
-    return ;
+    return document.getElementById("Hacker").value;
   }
-  else if (classHealth == "Engineer")
+  else if (document.getElementById("Engineer").checked)
   {
-    return ;
+    return document.getElementById("Engineer").value;
   }
   else
   {
-    return ;
+    return document.getElementById("SpaceCowboy").value;
   }
 }
 function addItemInventory(item)
@@ -73,8 +70,14 @@ function addItemInventory(item)
 
 }
 
+function createPlayer()
+{
+  var player = new playerObject(document.getElementById("name").value, charHealth(), document.getElementById("charClass"), inventory, stats);
+}
+
 function nameOutput()
 {
   var nameInput = document.getElementById("name").value;
-  alert(nameInput);
+  var classSelect = charStart();
+  alert(nameInput + " " + classSelect);
 }
