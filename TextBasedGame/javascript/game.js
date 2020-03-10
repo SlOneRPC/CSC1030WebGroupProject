2fa33098a99ae0b9847c8bc9c632668de6c98c12
mdeane07 <michael.deane01@gmail.com>
2020-03-10 16:06:39 +0000

    Merged Changes

--- TextBasedGame/javascript/game.js
+++ TextBasedGame/javascript/game.js
@@ -1,13 +1,15 @@
+var player = new playerObject(document.getElementById("name").value, charHealth(), document.getElementById("charClass") inventory, stats);
+var stats = new statsObject();
 var stats = new statsObject(1, 1, 1, 1);
 var inventory = [];
+var item = new itemObject();
+var interactable =  new interactableObject();
+
 var item = new itemObject(1,1,11);
 var interactable =  new interactableObject(4);
 
 var rooms = [];
 
-var enemies = [];
-var exits = [];
-var roomItems = [];
 var interactableRoomObjects = [];
 var verb = [];
 
@@ -19,54 +21,29 @@ function statsObjects(areasExplored, endingAchieved, enemiesDefeated, timeLeft)
 {
 
 }
-function roomObject(type, description, enemies, exits, roomItems, interactableRoomObjects)
-{
-
-}
-function itemObject(name, type, interactions)
-{
-
-}
-function interactableObject(verb)
-{
-
-}
-
-function charHealth()
-{
-  var className = document.getElementById("charClass");
-  if (className == "Hacker")
-  {
-    return 100;
-  }
-  else if (classHealth == "Engineer")
-  {
-    return 100;
-  }
-  else
-  {
-    return 120;
-  }
-}
 
 function charStart()
 {
+  var className = document.getElementById("charClass");
+  if (className == "Hacker")
   if (document.getElementById("Hacker").checked)
   {
+    return ;
     return document.getElementById("Hacker").value;
   }
+  else if (classHealth == "Engineer")
   else if (document.getElementById("Engineer").checked)
   {
+    return ;
     return document.getElementById("Engineer").value;
   }
   else
   {
+    return ;
     return document.getElementById("SpaceCowboy").value;
   }
 }
 function addItemInventory(item)
-{
-
 
 }
 
@@ -78,6 +55,7 @@ function createPlayer()
 function nameOutput()
 {
   var nameInput = document.getElementById("name").value;
+  alert(nameInput);
   var classSelect = charStart();
   alert(nameInput + " " + classSelect);
 }
