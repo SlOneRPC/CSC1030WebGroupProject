68f015a34b96fce656a3a0646ec50f1fab6193dc
dmerron658 <david.merron112@gmail.com>
2020-03-16 14:29:40 +0000

    Fix for undefined display

--- TextBasedGame/javascript/game.js
+++ TextBasedGame/javascript/game.js
@@ -1,6 +1,6 @@
 var player = createPlayerObject("Luke", 100, "Hacker", "", [], "yes");
-var item = createItemObject("Gun" , "Weapon" , );
 var rooms = [];
+var roomDescriptions = [];
 addRooms();
 
 
@@ -10,7 +10,6 @@ function addDescriptions(){
   var quartersDesc = "******, you, have just been sleeping after a long shift fixing the troublesome vacuum seal on\n the ship, the SS Viper, when you are woken up. "
   + "It is a subtle vibration at first, enough to nudge you into a semi-awakened state. It escalates more and more, with bedding from other beds falling onto the floor, and the very mattress you are on leaping to and fro. "
   + "You are thrown suddenly to the floor with a bang that could wake the dead.You stumble to your feet, bleary-eyed, with no idea what’s going on, but whatever it is, it isn’t going to be fun.";
-  var
   roomDescriptions.push(quartersDesc);
   //alert(roomDescriptions[0]);
 }
@@ -58,15 +57,15 @@ function addRooms()
   rooms.push(hallway04);
 
   // if statement used to assign correct first rooms
-  if (character == "Hacker"){
-    player.currentRoom = computerLab;
-  }
-  else if (character == "SpaceCowboy"){
-    player.currentRoom = armory;
-  }
-  else{
+  //if (character == "Hacker"){
+    //player.currentRoom = computerLab;
+  //}
+//  else if (character == "SpaceCowboy"){
+//    player.currentRoom = armory;
+//  }
+//  else{
     player.currentRoom = quarters;
-  }
+//  }
 }
 
 function outputCurrentAndNextRoom()
