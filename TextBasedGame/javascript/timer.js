d21b9d4b5c0e2b0bf49983a160543ee516b72983
dmerron658 <david.merron112@gmail.com>
2020-03-19 10:28:11 +0000

    5 min timer

--- TextBasedGame/javascript/timer.js
+++ TextBasedGame/javascript/timer.js
@@ -4,7 +4,7 @@ var passedValue="chess";
 
 function timerOperation(){
   // Set the date we're counting down to
-  var startTime = new Date().getTime() + (10*1000);
+  var startTime = new Date().getTime() + (5*60*1000);
 
   // Update the count down every 1 second
   var x = setInterval(function() {
