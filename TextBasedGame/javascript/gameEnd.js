60c327388bf6f9de6489f71e0a8c8a45a993dff4
dmerron658 <david.merron112@gmail.com>
2020-03-20 17:19:18 +0000

    start of game restart content

--- TextBasedGame/javascript/gameEnd.js
+++ TextBasedGame/javascript/gameEnd.js
@@ -1,3 +1,4 @@
+// this method displays the stats of the last game run to the user
 function gameOverStats(){
 
   var stats = JSON.parse(sessionStorage.getItem('stats'));
@@ -6,4 +7,9 @@ function gameOverStats(){
   document.getElementById("enemiesKilled").textContent = stats.enemiesDefeated;
   document.getElementById("roomsEntered").textContent = stats.areasExplored;
   document.getElementById("itemsCollected").textContent = stats.itemsCollected;
+}
+
+// method used to restart the game
+function restartGame(){
+
 }
