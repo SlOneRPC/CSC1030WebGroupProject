0992de2e122cf79e6c255ab636bd7762a232f163
dmerron658 <david.merron112@gmail.com>
2020-04-21 15:50:24 +0100

    acheivements display

--- TextBasedGame/javascript/gameEnd.js
+++ TextBasedGame/javascript/gameEnd.js
@@ -2,6 +2,7 @@
 function gameFinishedStats(){
 
   // formatting the page appropriately
+
   if(sessionStorage.getItem("ending") === "true"){ // - user beats the game
     gameWon();
   }
@@ -30,4 +31,56 @@ function gameWon(){
 // method used to restart the game - loads start page  - edit start menu to include user name - sessionStorage variable maybe
 function restartGame(){
   window.location.href = "test.html";
+}
+
+function achievements(){
+  // speed demon achievement
+  if(sessionStorage.getItem("timeSpent")>180){
+    document.getElementById("speedDemon").style.color = "green";
+  }
+  else{
+    document.getElementById("speedDemon").style.color = "red";
+  }
+
+  if(stats.enemiesDefeated > 10){
+    document.getElementById("warrior").style.color = "green";
+  }
+  else{
+    document.getElementById("warrior").style.color = "red";
+  }
+
+  if(stats.areasExplored > 20){
+    document.getElementById("explorer").style.color = "green";
+  }
+  else{
+    document.getElementById("explorer").style.color = "red";
+  }
+
+  if(stats.itemsCollected > 10){
+    document.getElementById("collector").style.color = "green";
+  }
+  else{
+    document.getElementById("collector").style.color = "red";
+  }
+
+  //digital love
+  //if()
+}
+
+function achievementInfo(title){
+  if(title === "speedDemon"){
+
+  }
+  else if(title === "warrior"){
+
+  }
+  else if(title ==="explorer"){
+
+  }
+  else if(title === "collector"){
+
+  }
+  else if(title === "digitalLove"){
+    
+  }
 }
